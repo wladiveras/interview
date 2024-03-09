387563b62e7b585c05709fa6eb89e84da87cc819
wladi Veras <wladinart@gmail.com>
2024-03-09 14:19:19 -0300

    add a router system

--- src/index.php
+++ src/index.php
@@ -2,18 +2,29 @@
 
 require_once('autoload.php');
 
-
-
-
 $url = $_SERVER['REQUEST_URI'];
 
-if (preg_match('/\/([a-z]+)/i', $url, $matches)) {
-    $controllerName = ucfirst($matches[1]);
+$routes = [
+    '/home' => ['HomeController', 'index'],
+    '/about' => ['AboutController', 'index'],
+    '/contact' => ['ContactController', 'index'],
+];
+
+$controllerName = 'HomeController';
+$actionName = 'index';
+
+foreach ($routes as $route => $handler) {
+    if (preg_match('#^' . $route . '$#i', $url, $matches)) {
+        $controllerName = $handler[0];
+        $actionName = $handler[1];
+        break;
+    }
 }
 
-if (preg_match('/\/([a-z]+)/i', $url, $matches)) {
-    $action_name = $matches[1];
+if (!empty($controllerName) && !empty($actionName)) {
+    $controller = new $controllerName();
+    $controller->$actionName($matches);
+} else {
+    echo '404 Not Found';
 }
 
-$controller = new HomeController();
-$controller->index();
