a8482f4c590267cbf5d7ae34440a9069c078b598
wladi Veras <wladinart@gmail.com>
2024-03-09 13:16:07 -0300

    Add meta charset, jQuery, and font-awesome

--- src/views/home.view.php
+++ src/views/home.view.php
@@ -2,10 +2,20 @@
 <html>
 
 <head>
+    <meta charset="utf-8">
     <title>
         Welcome
         <?php echo $data['name']; ?>
     </title>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+    <script>
+        $(document).ready(function () {
+            $("h1").css("color", "#0088ff");
+        });
+    </script>
 </head>
 
 <body>
