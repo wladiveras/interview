187dd4702ba881bc9ecb927893309fd642218d39
wladi Veras <wladinart@gmail.com>
2024-03-12 01:52:33 -0300

    Add database connection to User class constructor

--- src/models/User.php
+++ src/models/User.php
@@ -2,6 +2,12 @@
 
 class User
 {
+    private $database;
+
+    public function __construct()
+    {
+        $this->database = new Database();
+    }
     public function getData()
     {
         // TODO: ADD A REAL SQL
